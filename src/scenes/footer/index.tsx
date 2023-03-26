521e422b9761fe7fcd032c54308dd933417aaccb
kevinsmasters <gmail@kevinmasters.com>
2023-03-26 19:56:04 -0400

    progress on footer component

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,26 @@
+import Logo from "@/assets/Logo.png";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Nascetur aenean massa auctor tincidunt. Iaculis potenti amet egestas
+            ultrices consectetur adipiscing ultricies enim.{" "}
+          </p>
+          <p>&copy; Blah blah all rights</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+        </div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
